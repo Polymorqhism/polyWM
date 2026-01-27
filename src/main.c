f6c132689aa49268e49f671c72eed55565403acb
poly <99827144+Polymorqhism@users.noreply.github.com>
2026-01-27 19:04:05 +0530

    fix: gitignore not ignoring buffer files

--- src/main.c
+++ src/main.c
@@ -1,9 +1,17 @@
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 #include <X11/Xos.h>
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
 
-Display *dis;
-int screen;
-Window win;
-GC gc;
+
+int main()
+{
+    Display *dis;
+    int screen;
+    Window win;
+    GC gc;
+
+    if(!(dis = XOpenDisplay(0x0))) return 1;
+}
+
 
