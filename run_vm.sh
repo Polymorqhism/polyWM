b2a4b31f7d846c41597828566dc54a2edd500a55
poly <99827144+Polymorqhism@users.noreply.github.com>
2026-01-27 19:00:37 +0530

    add: setup VM run, src

--- run_vm.sh
+++ run_vm.sh
@@ -1,12 +1,10 @@
+#!/bin/bash
+
 qemu-system-x86_64 \
   -enable-kvm \
   -m 2048 \
   -cpu host \
   -smp 2 \
   -drive file=arch.qcow2,format=qcow2 \
-  -cdrom archlinux.iso \
-  -boot order=d \
-  -device virtio-net-pci \
-  -netdev user,id=net0 \
-  -device virtio-gpu-pci \
-  -display gtk
+  -device virtio-net-pci,netdev=net0 \
+  -netdev user,id=net0,hostfwd=tcp::2222-:22,hostfwd=tcp::1234-:1234
---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <X11/Xos.h>
+
+Display *dis;
+int screen;
+Window win;
+GC gc;
+
