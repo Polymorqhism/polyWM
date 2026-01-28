a6a6579ba0d533d6249b7293610312a5bfa6bacc
poly <99827144+Polymorqhism@users.noreply.github.com>
2026-01-28 20:19:09 +0530

    feat: add workspaces, bottom text

--- config.h
+++ config.h
@@ -4,16 +4,23 @@
 #include "util.h"
 
 #define BG_COLOR "#293329"
-
+#define BAR_HEIGHT 20
+#define WORKSPACES 6
 
 void spawn_terminal();
 void quit();
 void dmenu();
 void close();
+void change_focus();
+void workspace_front();
+void workspace_back();
 
 static Keybind keys[] = {
     { XK_Return, Mod4Mask, spawn_terminal },
     { XK_d, Mod4Mask, dmenu },
     { XK_p, Mod4Mask, quit },
     { XK_q, Mod4Mask, close },
+    { XK_f, Mod4Mask, change_focus },
+    { XK_j, Mod4Mask, workspace_back },
+    { XK_k, Mod4Mask, workspace_front },
 };
--- polywm.c
+++ polywm.c
@@ -1,22 +1,63 @@
 #include <X11/Xlib.h>
+#include <string.h>
+#include <stdio.h>
 #include <X11/cursorfont.h>
+#include <stdlib.h>
 #include <X11/Xutil.h>
 #include <X11/keysym.h>
 
 #include "config.h"
 
 
-Window windows[2];
-int win_count = 0;
+char buf[64];
+
+int width, height;
+int current_workspace = 0;
+Window windows[WORKSPACES][2] = { { 0 } };
+int win_count[WORKSPACES] = { 0 };
 
 Display *dis;
 Window root;
 XEvent ev;
+GC gc;
+XColor xcolor;
+int screen_number;
 
 
 void spawn_terminal() { system("alacritty &"); }
 void quit() { system("killall Xorg"); }
 void dmenu() { system("dmenu_run &"); }
+
+void tile_windows()
+{
+    /*
+       yes, i am getting dimensions every time i have to re-tile windows
+       call it bad practice, i call it 'safety' if the dimensions SOMEHOW change before re-tiling
+     */
+
+    int width, height;
+
+    width = XDisplayWidth(dis, screen_number);
+    height = XDisplayHeight(dis, screen_number);
+
+    if(win_count[current_workspace] == 1) {
+        XMoveResizeWindow(dis, windows[current_workspace][0], 0, 0, width, height-BAR_HEIGHT);
+    } else if(win_count[current_workspace] == 2) {
+        XMoveResizeWindow(dis, windows[current_workspace][0], 0, 0, width/2, height-BAR_HEIGHT);
+        XMoveResizeWindow(dis, windows[current_workspace][1], width/2, 0, width/2, height-BAR_HEIGHT);
+    }
+}
+
+void render_text(char *title)
+{
+    snprintf(buf, sizeof(buf), "Workspace: %d/%d : %s", current_workspace + 1, WORKSPACES, title);
+
+    XSetForeground(dis, gc, xcolor.pixel);
+    XFillRectangle(dis, root, gc, 0, height - BAR_HEIGHT, width, BAR_HEIGHT);
+
+    XSetForeground(dis, gc, WhitePixel(dis, screen_number));
+    XDrawString(dis, root, gc, 5, height - (BAR_HEIGHT/2), buf, strlen(buf));
+}
 
 void close()
 {
@@ -30,44 +71,86 @@ void close()
     }
 }
 
-void tile_windows(Display *dis, int screen_number)
+void load_workspace()
 {
-    /*
-       yes, i am getting dimensions every time i have to re-tile windows
-       call it bad practice, i call it 'safety' if the dimensions SOMEHOW change before re-tiling
-     */
+    char *title;
+    int height, width;
 
-    int width, height;
+    width = DisplayWidth(dis, screen_number);
+    height = DisplayHeight(dis, screen_number);
 
-    width = XDisplayWidth(dis, screen_number);
-    height = XDisplayHeight(dis, screen_number);
+    XFetchName(dis, ev.xcrossing.window, &title);
+    render_text(title);
+    for(int i = 0; i<win_count[current_workspace]; i++) {
+        XMapWindow(dis, windows[current_workspace][i]);
+    }
+    tile_windows();
+}
 
-    if(win_count == 1) {
-        XMoveResizeWindow(dis, windows[0], 0, 0, width, height-5);
-    } else if(win_count == 2) {
-        XMoveResizeWindow(dis, windows[0], 0, 0, width/2, height-5);
-        XMoveResizeWindow(dis, windows[1], width/2, 0, width/2, height-5);
+void unload_workspace()
+{
+    for(int i = 0; i<win_count[current_workspace]; i++) {
+        XUnmapWindow(dis, windows[current_workspace][i]);
     }
 }
 
+void workspace_back()
+{
+    if (current_workspace > 0) {
+        unload_workspace();
+        current_workspace--;
+        load_workspace();
+    }
+}
+
+void workspace_front()
+{
+    if (current_workspace < WORKSPACES - 1) {
+        unload_workspace();
+        current_workspace++;
+        load_workspace();
+    }
+}
+
+void change_focus()
+{
+    Window focus;
+    int revert;
+    XGetInputFocus(dis, &focus, &revert);
+
+
+    if(focus != root && focus != None && win_count[current_workspace] > 1) {
+        if(focus == windows[current_workspace][0]) {
+            XSetInputFocus(dis, windows[current_workspace][1], RevertToPointerRoot, CurrentTime);
+        } else {
+            XSetInputFocus(dis, windows[current_workspace][0], RevertToPointerRoot, CurrentTime);
+        }
+    }
+}
+
+
 void map_request_handler(Display *dis, XEvent *ev, int screen_number)
 {
-    if((win_count < 3)) {
-        windows[win_count] = ev->xmaprequest.window;
-        win_count++;
+    if((win_count[current_workspace] < 3)) {
+        windows[current_workspace][win_count[current_workspace]] = ev->xmaprequest.window;
+        win_count[current_workspace]++;
     }
 
     XSelectInput(dis, ev->xmaprequest.window, EnterWindowMask);
     XMapWindow(dis, ev->xmaprequest.window);
     XAddToSaveSet(dis, ev->xmaprequest.window);
     XSetInputFocus(dis, ev->xmaprequest.window, RevertToPointerRoot, CurrentTime);
-    tile_windows(dis, screen_number);
+    tile_windows();
 }
+
+
 
 int main()
 {
 
     if(!(dis = XOpenDisplay(NULL))) return 1;
+
+    screen_number = DefaultScreen(dis);
     root = DefaultRootWindow(dis);
 
     for(int i = 0; i<LENGTH(keys); i++) {
@@ -77,15 +160,23 @@ int main()
     Cursor cursor = XCreateFontCursor(dis, XC_left_ptr);
     XDefineCursor(dis, root, cursor);
 
-    int screen_number = DefaultScreen(dis);
 
     Colormap colormap = DefaultColormap(dis, screen_number);
 
-    XColor xcolor;
     XAllocNamedColor(dis, colormap, BG_COLOR, &xcolor, &xcolor);
     XSetWindowBackground(dis, root, xcolor.pixel);
+
+    XFontStruct *font = XLoadQueryFont(dis, "fixed");
+    gc = XCreateGC(dis, root, 0, NULL);
+    XSetFont(dis, gc, font->fid);
+    XSetForeground(dis, gc, WhitePixel(dis, screen_number));
     XClearWindow(dis, root);
 
+
+    width = XDisplayWidth(dis, screen_number);
+    height = XDisplayHeight(dis, screen_number);
+
+    XDrawString(dis, root, gc, 0, height-(BAR_HEIGHT/2), buf, strlen(buf));
     XSelectInput(dis, root, SubstructureRedirectMask | SubstructureNotifyMask);
 
 
@@ -112,16 +203,33 @@ int main()
 
         else if(ev.type == EnterNotify) {
             XSetInputFocus(dis, ev.xcrossing.window, RevertToPointerRoot, CurrentTime);
+            char *title;
+            XFetchName(dis, ev.xcrossing.window, &title);
+            render_text(title);
         }
 
         else if(ev.type == DestroyNotify) {
-            for(int i = 0; i < win_count; i++) {
-                if(windows[i] == ev.xdestroywindow.window) {
-                    windows[i] = windows[win_count -1];
-                    win_count--;
-                    tile_windows(dis, screen_number);
+            for(int i = 0; i < win_count[current_workspace]; i++) {
+                if(windows[current_workspace][i] == ev.xdestroywindow.window) {
+                    windows[current_workspace][i] = windows[current_workspace][win_count[current_workspace] -1];
+                    win_count[current_workspace]--;
+                    tile_windows();
                     if(win_count > 0) {
-                        XSetInputFocus(dis, windows[0], RevertToPointerRoot, CurrentTime);
+                        XSetInputFocus(dis, windows[current_workspace][0], RevertToPointerRoot, CurrentTime);
+                    }
+                    break;
+                }
+            }
+        }
+
+        else if(ev.type == UnmapNotify) {
+            for(int i = 0; i < win_count[current_workspace]; i++) {
+                if(windows[current_workspace][i] == ev.xunmap.window) {
+                    windows[current_workspace][i] = windows[current_workspace][win_count[current_workspace] -1];
+                    win_count[current_workspace]--;
+                    tile_windows();
+                    if(win_count[current_workspace] > 0) {
+                        XSetInputFocus(dis, windows[current_workspace][0], RevertToPointerRoot, CurrentTime);
                     }
                     break;
                 }
